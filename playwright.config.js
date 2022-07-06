acbba4f12ffc965bb890c72ad99e7320ef2ada6f
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-06 16:03:17 -0400

    :construction: Work in CI

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ const config = {
   expect: {
     timeout: 5000,
   },
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
