487e1dd59614d16b344c2dc3f7e7bcdfe2b4a938
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-06 18:13:03 -0400

    :memo: Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  extends: ["eslint:recommended", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
