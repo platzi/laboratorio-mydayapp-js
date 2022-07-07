30575985ba5752a0b0b25f4a822a68ff8416e89f
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-06 20:03:44 -0400

    :iphone: Work

---
+++ src/js/helpers.js
@@ -0,0 +1,17 @@
+export const getURLHash = () => document.location.hash.replace(/^#\//, "");
+
+export const delegate = (el, selector, event, handler) => {
+  el.addEventListener(event, (e) => {
+    if (e.target.matches(selector)) handler(e, el);
+  });
+};
+
+export const insertHTML = (el, markup) => {
+  el.insertAdjacentHTML("afterbegin", markup);
+};
+
+export const emptyElement = (el) => {
+  while (el.hasChildNodes()) {
+    el.removeChild(el.lastChild);
+  }
+};
