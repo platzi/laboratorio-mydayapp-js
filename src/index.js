8b590ca20d6b36aed167b9c17b6f5cd64c515642
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-22 18:00:22 -0500

    :sparkles: Clear completed todos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./css/base.css";
-import { addTodo, currentTodos } from "./js/store.manager.js";
+import {
+  addTodo,
+  clearCompletedTodos,
+  currentTodos,
+} from "./js/store.manager.js";
 import { createTodoUI, updateUI } from "./js/ui.manager";
 
 const newTodoInput = document.querySelector("input.new-todo");
 const todosList = document.querySelector(".todo-list");
+const clearCompletedButton = document.querySelector("button.clear-completed");
 
 // ### ### ### ###
 // Events
@@ -20,6 +25,11 @@ newTodoInput.addEventListener("keydown", (eventKey) => {
       updateUI(currentTodos);
     }
   }
+});
+
+clearCompletedButton.addEventListener("click", () => {
+  clearCompletedTodos();
+  updateUI(currentTodos);
 });
 
 document.addEventListener("DOMContentLoaded", () => {
