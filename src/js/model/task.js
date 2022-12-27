8a8852357abe0b01280d38812aab02162e04d289
Jorge Sanabria <jorgesanux1@gmail.com>
2022-12-26 20:25:04 -0500

    feat(model):Added Task and TaskList models
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

---
+++ src/js/model/task.js
@@ -0,0 +1,28 @@
+export class Task {
+  #id;
+  #title;
+  #completed;
+  constructor(id, title, completed = false) {
+    this.#id = id;
+    this.#title = title;
+    this.#completed = completed;
+  }
+  get id() {
+    return this.#id;
+  }
+  get title() {
+    return this.#title;
+  }
+  set title(title) {
+    this.#title = title;
+  }
+  isCompleted() {
+    return this.#completed;
+  }
+  markAsCompleted() {
+    this.#completed = true;
+  }
+  markAsPending() {
+    this.#completed = false;
+  }
+}
