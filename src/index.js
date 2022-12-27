c025d084778dbd12142f1f05a903c8f292899689
Jorge Sanabria <jorgesanux1@gmail.com>
2022-12-26 20:26:55 -0500

    feat(view): Added initial logic to view
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import "./css/base.css";
+import { Controller } from "./js/controller";
 
-import { sayHello } from "./js/utils";
-
-console.log(sayHello("Hello"));
+class View {
+  #controller;
+  constructor() {
+    this.#controller = new Controller();
+    this.elementFooter = document.querySelector(".footer");
+    this.elementMain = document.querySelector(".main");
+    this.elementMainInput = document.querySelector(".new-todo");
+  }
+  init() {
+    this.render();
+  }
+  showElement(element, visible) {
+    if (visible) {
+      element.classList.remove("hidden");
+    } else {
+      element.classList.add("hidden");
+    }
+  }
+  render() {
+    const thereAreTasks = this.#controller.thereAreTasks();
+    this.showElement(this.elementFooter, thereAreTasks);
+    this.showElement(this.elementMain, thereAreTasks);
+  }
+}
+new View().init();
