6046a96b7924e0859a278f24fe194ffd4db21474
jhoandvid <rojasalmariojhoandvid@gmail.com>
2022-12-28 09:43:59 -0500

    list html in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,109 @@
+
 import "./css/base.css";
+import { v4 as uuidv4 } from 'uuid';
 
-import { sayHello } from "./js/utils";
 
-console.log(sayHello("Hello"));
+import {ocultMainAndFooter, counterItem} from './js/utils'
+
+
+
+
+
+
+//ocultar los elementos si estan vacios 
+
+
+
+//input principal;
+const inputPrincipal=document.querySelector('.new-todo');
+inputPrincipal.autofocus;
+
+
+/* {id:uuidv4(), title:'buy some cheese', complete:false},
+{id:uuidv4(), title:'feed the cat', complete:false},
+{id:uuidv4(), title:'book a doctors appointment', complete:false}, */
+
+let items=[];
+ocultMainAndFooter(items);
+inputPrincipal.addEventListener('change',(ele)=>{
+
+    const title=ele.target.value.trim();
+    const data={id:uuidv4(), title, completed:false};
+    items.push({...data});
+    counterItem(items.length); 
+    localStorage.setItem("mydayapp-js",JSON.stringify(items));
+    ocultMainAndFooter(items);  
+   inputPrincipal.value="";
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <li class="completed">
+<div class="view">
+  <input class="toggle" type="checkbox" checked />
+  <label>Learn JavaScript</label>
+  <button class="destroy"></button>
+</div>
+<input class="edit" value="Learn JavaScript" />
+</li>
+
+
+<li>
+<div class="view">
+  <input class="toggle" type="checkbox" />
+  <label>Buy a unicorn</label>
+  <button class="destroy"></button>
+</div>
+<input class="edit" value="Buy a unicorn" />
+</li>
+
+
+<li class="editing">
+<div class="view">
+  <input class="toggle" type="checkbox" />
+  <label>Make dishes</label>
+  <button class="destroy"></button>
+</div>
+<input class="edit" value="Make dishes" />
+</li> */}
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,89 @@
-export const sayHello = (text) => {
-  return text;
-};
+
+
+
+
+export const listTodo = (items) => {
+  const todoList = document.querySelector('.todo-list');
+
+  todoList.innerHTML = " ";
+
+  items.forEach(ele => {
+    //Creación de elementos
+    const li = document.createElement('li');
+
+    todoList.append(li);
+    const divView = document.createElement('div');
+    divView.className += "view";
+    li.append(divView);
+
+    const inputCheckbox = document.createElement('input');
+    inputCheckbox.className += "toggle";
+    inputCheckbox.type = "checkbox";
+
+
+    inputCheckbox.addEventListener('change', ()=>{
+
+      inputCheckbox.toggleAttribute('checked');
+      li.classList.toggle("completed")
+
+
+      
+  /*     if(inputCheckbox.checked){
+        li.className+="completed"
+
+      }else{
+        li.classList.remove('completed')
+      }  */
+
+    })
+
+   /*  if(inputCheckbox.checked){
+      li.className+=" completed"
+    } */
+
+    inputCheckbox.checked=ele.completed;
+
+    const label = document.createElement('label');
+    label.innerText = `${ele.title}`;
+
+    const buttonDestroy = document.createElement('button');
+    buttonDestroy.className += "destroy";
+
+    divView.append(inputCheckbox, label, buttonDestroy);
+
+    //Creación al momento de click para editar;
+    const inputEdit = document.createElement('input');
+    inputEdit.className += "edit";
+    li.append(inputEdit);
+  })
+}
+
+
+export const counterItem=(total)=>{
+    document.querySelector('strong').innerText=`${total}`
+}
+
+
+
+
+
+
+export const ocultMainAndFooter = (items) => {
+  const main = document.querySelector('.main');
+  const footer = document.querySelector('.footer');
+  if (items.length < 1) {
+    main.classList += " ocult";
+    footer.classList += " ocult";
+  } else {
+    main.classList.remove("ocult");
+    footer.classList.remove("ocult");
+    listTodo(items);
+  }
+
+}
+
+
+
+
+
+
