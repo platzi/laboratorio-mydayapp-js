2c507d8106930be1eb359f8c3022b809d651cd11
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2022-12-29 10:49:15 -0600

    feat: Hide main and footer when no tasks

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import "./css/base.css";
 
 import { sayHello } from "./js/utils";
 
+function updateUI() {
+    let todosLength = document.getElementsByClassName('todo-list')[0].childElementCount
+    let main = document.getElementsByClassName('main')[0]
+    let footer = document.getElementsByClassName('footer')[0]
+
+    if (todosLength === 0) {
+        main.style.display = 'none'
+        footer.style.display = 'none'
+    }
+    else {
+        main.style.display = 'block'
+        footer.style.display = 'block'
+    }
+}
+updateUI()
 console.log(sayHello("Hello"));
