747d98afb091def8f01fd1d23b1db9555323158f
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-01-01 19:28:49 -0600

    feat: Add handler for edit task

--- src/js/crud.js
+++ src/js/crud.js
@@ -10,19 +10,50 @@ function addTodo({title}) {
     todo.setAttribute('id', `item_${TODO.id}`)
 
     var containerDiv = document.createElement('div')
+    containerDiv.classList.add('view')
 
     var checkbox = document.createElement('input')
     checkbox.setAttribute('class', 'toggle')
     checkbox.setAttribute('type', 'checkbox')
     checkbox.setAttribute('value', TODO.id)
     checkbox.addEventListener('click', () => {
-        let todoItem = todo
-        todoItem.classList.toggle('completed')
+        todo.classList.toggle('completed')
+    })
+
+    // Handles edit
+    let edit = document.createElement('input')
+    edit.setAttribute('type', 'edit')
+    edit.setAttribute('class', 'hidden')
+    edit.value = TODO.title
+    edit.addEventListener('keydown', (event) => {
+        if (event.code === 'Enter' || event.code === "Escape"){
+            todo.classList.remove('editing')
+            checkbox.classList.toggle('hidden')
+            edit.classList.add('hidden')
+            edit.classList.remove('edit')
+            label.innerText = edit.value
+        }
+    })
+    edit.addEventListener('blur', (event) => {
+        console.log(edit.value)
+        todo.classList.remove('editing')
+        checkbox.classList.toggle('hidden')
+        edit.classList.add('hidden')
+        edit.classList.remove('edit')
+        label.innerText = edit.value
+    })
+
+    todo.addEventListener('dblclick', () => {
+        todo.classList.add('editing')
+        checkbox.classList.toggle('hidden')
+        edit.classList.remove('hidden')
+        edit.classList.add('edit')
     })
 
     var label = document.createElement('label')
     label.innerText = TODO.title
 
+    // Handles remove
     var button = document.createElement('button')
     button.setAttribute('class', 'destroy')
     button.setAttribute('id', TODO.id)
@@ -35,6 +66,7 @@ function addTodo({title}) {
     containerDiv.appendChild(label)
     containerDiv.appendChild(button)
     todo.appendChild(containerDiv)
+    todo.appendChild(edit)
     
     let todos = document.querySelector('.todo-list')
     todos.appendChild(todo);
