4713d782e8b24c8e2f57718b309375cdfe01071e
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-01-01 19:28:11 -0600

    refactor: Use mutationObserver for main and footer

--- src/index.js
+++ src/index.js
@@ -6,20 +6,23 @@ import { sayHello } from "./js/utils";
 let TODOS = []
 let inputTodo = document.getElementsByClassName('new-todo')[0]
 
-function updateUI() {
-    let todosLength = document.getElementsByClassName('todo-list')[0].childElementCount
+let observer = new MutationObserver(function(mutations) {
     let main = document.getElementsByClassName('main')[0]
     let footer = document.getElementsByClassName('footer')[0]
+    mutations.forEach(function(mutation) {
+      if (mutation.target.childElementCount === 0) {
+        main.classList.add('hidden')
+        footer.classList.add('hidden')
+      } else {
+        main.classList.remove('hidden')
+        footer.classList.remove('hidden')
+      }
+    });
+  });
 
-    if (todosLength === 0) {
-        main.style.display = 'none'
-        footer.style.display = 'none'
-    }
-    else {
-        main.style.display = 'block'
-        footer.style.display = 'block'
-    }
-}
+  observer.observe(document.getElementsByClassName("todo-list")[0], {
+    childList: true
+  });
 
 inputTodo.addEventListener('keypress', (event) => {
     const errorMessage = document.querySelector('.error-message');
@@ -38,8 +41,6 @@ inputTodo.addEventListener('keypress', (event) => {
         }
     }
 
-    updateUI()
 })
 
-updateUI()
 console.log(sayHello("Hello"));
