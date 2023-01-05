a8cb58d36a2933c463dc1c5d377c6c54fed4512e
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-01-05 14:24:43 -0600

    refactor: Lint code

--- src/index.js
+++ src/index.js
@@ -1,71 +1,79 @@
 import "./css/base.css";
-import { addTodo, updateCounter, getLocalStorage, clearCompletedTasks, checkForCompletedTask, getFilterByRoute, filterTODOSByRoute} from './js/crud.js'
+import {
+  addTodo,
+  updateCounter,
+  getLocalStorage,
+  clearCompletedTasks,
+  checkForCompletedTask,
+  getFilterByRoute,
+  filterTODOSByRoute,
+} from "./js/crud.js";
 
 import { sayHello } from "./js/utils";
 
-let inputTodo = document.getElementsByClassName('new-todo')[0]
-let clearCompletedButton = document.getElementsByClassName('clear-completed')[0]
-let filterButtons = document.getElementsByClassName('filters')[0].querySelectorAll('li a')
+let inputTodo = document.getElementsByClassName("new-todo")[0];
+let clearCompletedButton =
+  document.getElementsByClassName("clear-completed")[0];
+let filterButtons = document
+  .getElementsByClassName("filters")[0]
+  .querySelectorAll("li a");
 
-let observer = new MutationObserver(function(mutations) {
-    let main = document.getElementsByClassName('main')[0]
-    let footer = document.getElementsByClassName('footer')[0]
-    mutations.forEach(function(mutation) {
-      if (mutation.target.childElementCount === 0) {
-        main.classList.add('hidden')
-        footer.classList.add('hidden')
-      } else {
-        main.classList.remove('hidden')
-        footer.classList.remove('hidden')
-      }
-      checkForCompletedTask()
-      updateCounter()
-    });
-  });
-
-  observer.observe(document.getElementsByClassName("todo-list")[0], {
-    childList: true
-  });
-
-let filter = getFilterByRoute()
-let TODOS = getLocalStorage(filter)
-TODOS = Array.from(TODOS)
-TODOS.forEach(element => {
-    addTodo(element)
-});
-
-filterButtons.forEach((button) =>
-{
-    button.addEventListener('click', (event) => {
-        filterButtons.forEach(element => element.classList.remove('selected'))
-        button.classList.add('selected')
-        filterTODOSByRoute(event.target.href)
-    })
-}
-)
-
-inputTodo.addEventListener('keypress', (event) => {
-    const errorMessage = document.querySelector('.error-message');
-
-    if (event.key === 'Enter')
-    {
-        if (inputTodo.value.length !== 0){
-            event.preventDefault()
-            addTodo({title: inputTodo.value.trim()})
-            inputTodo.value = ''
-            errorMessage.textContent = ''
-        }
-        else {
-            event.target.setCustomValidity('La tarea debe contener al menos un caracter')
-            errorMessage.textContent = event.target.validationMessage;
-        }
+let observer = new MutationObserver(function (mutations) {
+  let main = document.getElementsByClassName("main")[0];
+  let footer = document.getElementsByClassName("footer")[0];
+  mutations.forEach(function (mutation) {
+    if (mutation.target.childElementCount === 0) {
+      main.classList.add("hidden");
+      footer.classList.add("hidden");
+    } else {
+      main.classList.remove("hidden");
+      footer.classList.remove("hidden");
     }
-
-})
-
-window.addEventListener('popstate', function(event) {
-    filterTODOSByRoute(event.target.location.href)
+    checkForCompletedTask();
+    updateCounter();
+  });
 });
 
-clearCompletedButton.addEventListener('click', () => clearCompletedTasks())
+observer.observe(document.getElementsByClassName("todo-list")[0], {
+  childList: true,
+});
+
+let filter = getFilterByRoute();
+let TODOS = getLocalStorage(filter);
+TODOS = Array.from(TODOS);
+TODOS.forEach((element) => {
+  addTodo(element);
+});
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    filterButtons.forEach((element) => element.classList.remove("selected"));
+    button.classList.add("selected");
+    filterTODOSByRoute(event.target.href);
+  });
+});
+
+inputTodo.addEventListener("keypress", (event) => {
+  const errorMessage = document.querySelector(".error-message");
+
+  if (event.key === "Enter") {
+    if (inputTodo.value.length !== 0) {
+      event.preventDefault();
+      addTodo({ title: inputTodo.value.trim() });
+      inputTodo.value = "";
+      errorMessage.textContent = "";
+    } else {
+      event.target.setCustomValidity(
+        "La tarea debe contener al menos un caracter"
+      );
+      errorMessage.textContent = event.target.validationMessage;
+    }
+  }
+});
+
+window.addEventListener("popstate", function (event) {
+  filterTODOSByRoute(event.target.location.href);
+});
+
+clearCompletedButton.addEventListener("click", () => clearCompletedTasks());
 console.log(sayHello("Hello"));
--- src/js/crud.js
+++ src/js/crud.js
@@ -1,194 +1,201 @@
-let TODOS = getLocalStorage()
+let TODOS = getLocalStorage();
 
-function addTodo({id, title, completed}) {
-    let TODO = {
-        id: id ? id : getNextID(),
-        title: title,
-        completed: completed ? completed : false,
+function addTodo({ id, title, completed }) {
+  let TODO = {
+    id: id ? id : getNextID(),
+    title: title,
+    completed: completed ? completed : false,
+  };
+  var todo = document.createElement("li");
+  todo.setAttribute("id", `item_${TODO.id}`);
+
+  var containerDiv = document.createElement("div");
+  containerDiv.classList.add("view");
+
+  var checkbox = document.createElement("input");
+  checkbox.setAttribute("class", "toggle");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("value", TODO.id);
+  checkbox.checked = TODO.completed;
+  if (checkbox.checked) todo.classList.toggle("completed");
+  checkbox.addEventListener("click", (event) => {
+    todo.classList.toggle("completed");
+    let item = TODOS.find((el) => el.id === Number(TODO.id));
+    item.completed = event.target.checked;
+    saveLocalStorage(TODOS);
+    updateCounter();
+    checkForCompletedTask();
+  });
+
+  // Handles edit
+  let edit = document.createElement("input");
+  edit.setAttribute("type", "edit");
+  edit.setAttribute("class", "hidden");
+  edit.value = TODO.title.trim();
+  edit.setAttribute("id", TODO.id);
+  edit.addEventListener("keydown", (event) => {
+    let originalValue = label.innerText;
+    if (event.code === "Enter") {
+      label.innerText = edit.value;
+      let todoIndex = TODOS.findIndex(
+        (todo) => todo.id === Number(event.target.id)
+      );
+      TODOS[todoIndex].title = edit.value.trim();
+      saveLocalStorage(TODOS);
+      event.target.value = event.target.value.trim();
+      todo.classList.remove("editing");
+      checkbox.classList.toggle("hidden");
+      edit.classList.add("hidden");
+      edit.classList.remove("edit");
+      toggleTodos(`item_${event.target.id}`);
+    } else if (event.code === "Escape") {
+      label.innerText = originalValue;
+      event.target.value = originalValue;
+
+      event.target.value = event.target.value.trim();
+      todo.classList.remove("editing");
+      checkbox.classList.toggle("hidden");
+      edit.classList.add("hidden");
+      edit.classList.remove("edit");
+      toggleTodos(`item_${event.target.id}`);
     }
-    var todo = document.createElement("li");
-    todo.setAttribute('id', `item_${TODO.id}`)
+  });
+  var label = document.createElement("label");
+  label.innerText = TODO.title;
+  label.addEventListener("dblclick", (event) => {
+    toggleTodos(event.path[2].id);
+    todo.classList.add("editing");
+    checkbox.classList.toggle("hidden");
+    edit.classList.remove("hidden");
+    edit.classList.add("edit");
+    edit.focus();
+  });
 
-    var containerDiv = document.createElement('div')
-    containerDiv.classList.add('view')
+  // Handles remove
+  var button = document.createElement("button");
+  button.setAttribute("class", "destroy");
+  button.setAttribute("id", TODO.id);
+  button.addEventListener("click", () => {
+    todo.remove();
+    removeFromLocalStorage(button.id);
+  });
 
-    var checkbox = document.createElement('input')
-    checkbox.setAttribute('class', 'toggle')
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.setAttribute('value', TODO.id)
-    checkbox.checked = TODO.completed
-    if(checkbox.checked ) todo.classList.toggle('completed')
-    checkbox.addEventListener('click', (event) => {
-        todo.classList.toggle('completed')
-        let item = TODOS.find(el => el.id === Number(TODO.id))
-        item.completed = event.target.checked
-        saveLocalStorage(TODOS)
-        updateCounter()
-        checkForCompletedTask()
-    })
+  containerDiv.appendChild(checkbox);
+  containerDiv.appendChild(label);
+  containerDiv.appendChild(button);
+  todo.appendChild(containerDiv);
+  todo.appendChild(edit);
 
-    // Handles edit
-    let edit = document.createElement('input')
-    edit.setAttribute('type', 'edit')
-    edit.setAttribute('class', 'hidden')
-    edit.value = TODO.title.trim()
-    edit.setAttribute('id', TODO.id)
-    edit.addEventListener('keydown', (event) => {
-        let originalValue = label.innerText
-        if (event.code === 'Enter'){
-            label.innerText = edit.value
-            let todoIndex = TODOS.findIndex(todo => todo.id === Number(event.target.id))
-            TODOS[todoIndex].title = edit.value.trim()
-            saveLocalStorage(TODOS)
-            event.target.value = event.target.value.trim()
-            todo.classList.remove('editing')
-            checkbox.classList.toggle('hidden')
-            edit.classList.add('hidden')
-            edit.classList.remove('edit')
-            toggleTodos(`item_${event.target.id}`)
-        }
-        else if(event.code === "Escape"){
-            label.innerText = originalValue
-            event.target.value = originalValue
-
-            event.target.value = event.target.value.trim()
-            todo.classList.remove('editing')
-            checkbox.classList.toggle('hidden')
-            edit.classList.add('hidden')
-            edit.classList.remove('edit')
-            toggleTodos(`item_${event.target.id}`)
-        }
-    })
-    var label = document.createElement('label')
-    label.innerText = TODO.title
-    label.addEventListener('dblclick', (event) => {
-        toggleTodos(event.path[2].id)
-        todo.classList.add('editing')
-        checkbox.classList.toggle('hidden')
-        edit.classList.remove('hidden')
-        edit.classList.add('edit')
-        edit.focus()
-    })
-
-    // Handles remove
-    var button = document.createElement('button')
-    button.setAttribute('class', 'destroy')
-    button.setAttribute('id', TODO.id)
-    button.addEventListener('click', () => {
-        todo.remove()
-        removeFromLocalStorage(button.id)
-    })
-
-
-    containerDiv.appendChild(checkbox)
-    containerDiv.appendChild(label)
-    containerDiv.appendChild(button)
-    todo.appendChild(containerDiv)
-    todo.appendChild(edit)
-    
-    let todos = document.querySelector('.todo-list')
-    todos.appendChild(todo);
-    addTask(TODO)
+  let todos = document.querySelector(".todo-list");
+  todos.appendChild(todo);
+  addTask(TODO);
 }
 
-function toggleTodos (id) {
-    let todos = Array.from(document.querySelectorAll('li'))
-    todos
-        .filter((todo) => todo.id !== id)
-        .map((element) => element.classList.toggle('hidden'))
+function toggleTodos(id) {
+  let todos = Array.from(document.querySelectorAll("li"));
+  todos
+    .filter((todo) => todo.id !== id)
+    .map((element) => element.classList.toggle("hidden"));
 }
 
-function updateCounter(pendingTasK){
-    let ul = document.getElementsByClassName('todo-list')[0]
-        let pendingTasks = Array.from(ul.children).filter((task) => !task.classList.contains('completed'))
+function updateCounter() {
+  let ul = document.getElementsByClassName("todo-list")[0];
+  let pendingTasks = Array.from(ul.children).filter(
+    (task) => !task.classList.contains("completed")
+  );
 
-        let todoCounter = document.getElementsByClassName('todo-count')[0]
-        todoCounter.children[0].innerHTML = pendingTasks.length
-        let itemsText = pendingTasks.length === 1 ? 'item' : 'items'
-        todoCounter.innerHTML = todoCounter.innerHTML.replace(/\bitem(s)?\b/, itemsText)
+  let todoCounter = document.getElementsByClassName("todo-count")[0];
+  todoCounter.children[0].innerHTML = pendingTasks.length;
+  let itemsText = pendingTasks.length === 1 ? "item" : "items";
+  todoCounter.innerHTML = todoCounter.innerHTML.replace(
+    /\bitem(s)?\b/,
+    itemsText
+  );
 }
 
-function checkForCompletedTask(){
-    let clearCompletedButton = document.getElementsByClassName('clear-completed')[0]
-    let ul = document.getElementsByClassName('todo-list')[0]
-    let pendingTasks = Array.from(ul.children).filter((task) => task.classList.contains('completed'))
-    if (pendingTasks.length >= 1){
-        clearCompletedButton.classList.remove('hidden')
-    }
-    else clearCompletedButton.classList.add('hidden')
+function checkForCompletedTask() {
+  let clearCompletedButton =
+    document.getElementsByClassName("clear-completed")[0];
+  let ul = document.getElementsByClassName("todo-list")[0];
+  let pendingTasks = Array.from(ul.children).filter((task) =>
+    task.classList.contains("completed")
+  );
+  if (pendingTasks.length >= 1) {
+    clearCompletedButton.classList.remove("hidden");
+  } else clearCompletedButton.classList.add("hidden");
 }
 
-function addTask(task){
-    let alreadyInTasks = TODOS.some((element) => element.id === task.id)
-    if (!alreadyInTasks){
-        TODOS.push(task)
-        saveLocalStorage(TODOS)
-    }
+function addTask(task) {
+  let alreadyInTasks = TODOS.some((element) => element.id === task.id);
+  if (!alreadyInTasks) {
+    TODOS.push(task);
+    saveLocalStorage(TODOS);
+  }
 }
 
-function saveLocalStorage(TODOS){
-    localStorage.setItem('mydayapp-js', JSON.stringify(TODOS))
+function saveLocalStorage(TODOS) {
+  localStorage.setItem("mydayapp-js", JSON.stringify(TODOS));
 }
 
-function getLocalStorage(route = ''){
-    let filter = route !== '' ? route : getFilterByRoute()
-    let filterPredicate = (task) => task.completed === (filter === 'completed')
-    let todos = JSON.parse(localStorage.getItem('mydayapp-js'))
-    if(filter !== '' && filter !== 'all') todos = todos.filter(filterPredicate)
-    return todos !== null ? todos : []
+function getLocalStorage(route = "") {
+  let filter = route !== "" ? route : getFilterByRoute();
+  let filterPredicate = (task) => task.completed === (filter === "completed");
+  let todos = JSON.parse(localStorage.getItem("mydayapp-js"));
+  if (filter !== "" && filter !== "all") todos = todos.filter(filterPredicate);
+  return todos !== null ? todos : [];
 }
 
-function removeFromLocalStorage(taskID){
-    TODOS = TODOS.filter(todo => todo.id !== Number(taskID))
-    localStorage.setItem('mydayapp-js', JSON.stringify(TODOS))
-
+function removeFromLocalStorage(taskID) {
+  TODOS = TODOS.filter((todo) => todo.id !== Number(taskID));
+  localStorage.setItem("mydayapp-js", JSON.stringify(TODOS));
 }
 
-function getNextID(){
-    let highestId = TODOS.reduce((max, object) => {
-        return object.id > max ? object.id : max;
-      }, 0);
-    return highestId + 1
+function getNextID() {
+  let highestId = TODOS.reduce((max, object) => {
+    return object.id > max ? object.id : max;
+  }, 0);
+  return highestId + 1;
 }
 
-function clearCompletedTasks(){
-    let completedTasks = document.querySelectorAll('li.completed')
-    completedTasks = Array.from(completedTasks)
-    console.log(completedTasks);
-    completedTasks.forEach(task => task.remove())
-    TODOS = TODOS.filter(task => !task.completed)
-    saveLocalStorage(TODOS)
-
+function clearCompletedTasks() {
+  let completedTasks = document.querySelectorAll("li.completed");
+  completedTasks = Array.from(completedTasks);
+  console.log(completedTasks);
+  completedTasks.forEach((task) => task.remove());
+  TODOS = TODOS.filter((task) => !task.completed);
+  saveLocalStorage(TODOS);
 }
 
-function getFilterByRoute(){
-    let filter = ''
-    let hash = window.location.hash
-    
-    if(hash.includes('pending')) return 'pending'
-    else if(hash.includes('completed')) return 'completed'
-    else filter = 'all'
-    return filter
+function getFilterByRoute() {
+  let filter = "";
+  let hash = window.location.hash;
+
+  if (hash.includes("pending")) return "pending";
+  else if (hash.includes("completed")) return "completed";
+  else filter = "all";
+  return filter;
 }
 
-function filterTODOSByRoute(route){
-    let ul = document.getElementsByClassName('todo-list')[0]
-    Array.from(ul.childNodes).forEach(child => child.remove())
+function filterTODOSByRoute(route) {
+  let ul = document.getElementsByClassName("todo-list")[0];
+  Array.from(ul.childNodes).forEach((child) => child.remove());
 
-    route = route.includes('pending') ? 'pending':
-            route.includes('completed') ? 'completed':
-            'all'
-    let filteredTask = getLocalStorage(route)
-    filteredTask.forEach((task) => addTodo(task))
+  route = route.includes("pending")
+    ? "pending"
+    : route.includes("completed")
+    ? "completed"
+    : "all";
+  let filteredTask = getLocalStorage(route);
+  filteredTask.forEach((task) => addTodo(task));
 }
 
 export {
-    addTodo,
-    updateCounter,
-    addTask,
-    getLocalStorage,
-    clearCompletedTasks,
-    checkForCompletedTask,
-    getFilterByRoute,
-    filterTODOSByRoute,
-}
+  addTodo,
+  updateCounter,
+  addTask,
+  getLocalStorage,
+  clearCompletedTasks,
+  checkForCompletedTask,
+  getFilterByRoute,
+  filterTODOSByRoute,
+};
