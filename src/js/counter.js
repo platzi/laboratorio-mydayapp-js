ef6685e55241d6bf84e400b5d58b368d9c3b5810
Carlos Lopez <carlosmlopez066@gmail.com>
2023-01-11 09:24:50 -0400

    fix counter

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,36 +1,22 @@
+import { todoList } from ".."
 export function counter(NumberConfiguration) {
   //localStorage
-  let localArr = JSON.parse(localStorage.getItem('mydayapp-js'))
-  let completedArr = JSON.parse(localStorage.getItem('mydayapp-js-completed'))
-  let pendingArr = JSON.parse(localStorage.getItem('mydayapp-js-pending'))
+  // let localArr = JSON.parse(localStorage.getItem('mydayapp-js'))
+  // let completedArr = JSON.parse(localStorage.getItem('mydayapp-js-completed'))
+  // let pendingArr = JSON.parse(localStorage.getItem('mydayapp-js-pending'))
 
   const span = document.querySelector(".todo-count")
   const strong = document.querySelector("strong")
-  console.log("param", NumberConfiguration);
+  const numberOfItem = todoList.filter(element => element.visible === true
+  );
+  console.log("lengthList", numberOfItem.length);
 
-  if (NumberConfiguration == 0) {
+  if (numberOfItem.length != 1) {
     span.innerHTML = ""
-
-    validate(localArr)
-  } else if (NumberConfiguration == 1) {
-    span.innerHTML = ""
-
-    validate(completedArr)
-
-  } else if (NumberConfiguration == 2) {
-    span.innerHTML = ""
-
-    validate(pendingArr)
-
-  }
-
-  function validate(ArrForValidate) {
-    if (ArrForValidate.length != 1) {
-      // span.innerHTML = ""
-      span.append(`${ArrForValidate.length} items`);
-    } else {
-      // span.innerHTML = "";
-      span.append(`${ArrForValidate.length} item`);
-    };
-  }
+    span.append(`${numberOfItem.length} items`);
+  } else {
+    span.innerHTML = "";
+    span.append(`${numberOfItem.length} item`);
+  };
 }
+
