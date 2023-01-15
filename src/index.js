e26985c2733b2c7764913bce10d10634556c79c2
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-01-15 17:22:16 -0500

    feat(newTodoInput): add interaction to the newTodoInput

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import "./css/base.css";
 
 import { sayHello } from "./js/utils";
+import { header, newTodoInput } from "./js/domElements";
 
-console.log(sayHello("Hello"));
+// console.log(sayHello("Hello"));
+// console.log(header);
+// console.log(newTodoInput);
+
+
+const getInputValue = (input) => {
+  return input.target.value;
+}
+
+let newTodoInputValue = getInputValue;
+
+newTodoInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter'){
+    console.log(newTodoInputValue(e));
+    e.target.value = '';
+  }
+});
---
+++ src/js/domElements.js
@@ -0,0 +1,6 @@
+// This file contains every default HTML element found in the inital index.html file where changes will be made depending on user's interactions with the app
+
+export const header = document.querySelector('.header');
+
+export const newTodoInput = document.querySelector('.new-todo');
+
