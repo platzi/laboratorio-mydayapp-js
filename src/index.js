1a0aef8f4ded42c313c7555817545dedb6526691
DavidTG7 <davidthekoa@gmail.com>
2023-02-24 14:51:36 -0500

    Create logic for creating a todo and cheking it
    Create logic for user input to generate a new todo
    
    Create logic to check a todo and update the list of all todos
    
    Create logic for render all todos from the list todos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import { mainFooterDisplayValidator } from "./js/utils";
-
+import {
+  mainFooterDisplayValidator,
+  generalTodoListGenerator,
+} from "./js/utils";
 import "./css/base.css";
 
 mainFooterDisplayValidator();
+
+let todos = [];
+
+generalTodoListGenerator(todos);
+
+const newTodoInput = document.querySelector(".new-todo");
+
+let newUserInput = "";
+
+newTodoInput.addEventListener("input", (e) => {
+  const userInput = e.target.value;
+  newUserInput = userInput;
+});
+
+newTodoInput.addEventListener("keypress", (e) => {
+  if (e.code === "Enter" || e.code === "NumpadEnter") {
+    if (newUserInput.length < 1) return;
+    todos.push({ id: todos.length, title: newUserInput, completed: false });
+    generalTodoListGenerator(todos);
+
+    e.target.value = "";
+    newUserInput = "";
+  }
+  return;
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -9,3 +9,50 @@ export const mainFooterDisplayValidator = () => {
     footerTarget.style.display = "none";
   }
 };
+
+export const elementGenerator = (element) => document.createElement(element);
+
+export const createNewTodo = (todo, index, todos) => {
+  const listElement = elementGenerator("li");
+
+  const viewDiv = elementGenerator("div");
+  viewDiv.className = "view";
+
+  const checkboxInput = elementGenerator("input");
+  checkboxInput.type = "checkbox";
+  checkboxInput.className = "toggle";
+  checkboxInput.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      listElement.className = "completed";
+      todos[index].completed = true;
+    } else {
+      listElement.classList.remove("completed");
+      todos[index].completed = false;
+    }
+    console.log(todos);
+  });
+
+  const todoLabel = elementGenerator("label");
+  todoLabel.innerText = todo;
+
+  const todoDeleteButton = elementGenerator("button");
+  todoDeleteButton.className = "destroy";
+
+  const editTodoInput = elementGenerator("input");
+  editTodoInput.className = "edit";
+  editTodoInput.value = todo;
+
+  viewDiv.append(checkboxInput, todoLabel, todoDeleteButton);
+  listElement.append(viewDiv, editTodoInput);
+
+  return listElement;
+};
+
+export const generalTodoListGenerator = (todos) => {
+  const todoListTarget = document.querySelector(".todo-list");
+  todoListTarget.innerHTML = "";
+
+  todos.forEach((item, index) => {
+    todoListTarget.append(createNewTodo(item.title, index, todos));
+  });
+};
