13ef97ffae03fefc6deccd10754d22a721c9be53
DavidTG7 <davidthekoa@gmail.com>
2023-02-24 15:41:18 -0500

    Create logic to edit each todo

--- src/js/utils.js
+++ src/js/utils.js
@@ -42,6 +42,10 @@ export const createNewTodo = (todo, index, todos) => {
 
   const todoLabel = elementGenerator("label");
   todoLabel.innerText = todo;
+  todoLabel.addEventListener("click", () => {
+    listElement.className = "editing";
+    editTodoInput.focus();
+  });
 
   const todoDeleteButton = elementGenerator("button");
   todoDeleteButton.className = "destroy";
@@ -53,6 +57,21 @@ export const createNewTodo = (todo, index, todos) => {
   const editTodoInput = elementGenerator("input");
   editTodoInput.className = "edit";
   editTodoInput.value = todo;
+
+  let editInputValue = "";
+
+  editTodoInput.addEventListener("input", (e) => {
+    const editValue = e.target.value;
+    editInputValue = editValue;
+  });
+  editTodoInput.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") listElement.classList.remove("editing");
+    if (e.key === "Enter" || e.key === "NumpadEnter") {
+      editTodoInput.value = editInputValue;
+      todos[index].title = editInputValue;
+      generalTodoListGenerator(todos);
+    }
+  });
 
   viewDiv.append(checkboxInput, todoLabel, todoDeleteButton);
   listElement.append(viewDiv, editTodoInput);
