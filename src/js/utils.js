e9c887646c6d59c99355294ff61d7f10251021a5
Sochoag <santiagodav@hotmail.es>
2023-03-05 14:39:27 -0500

    First Task: Header and Footer hidden If not task added

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,15 @@
-export const sayHello = (text) => {
-  return text;
+export const todoContainer = document.querySelector("ul.todo-list");
+export const mainContainer = document.querySelector(".main");
+export const footerContainer = document.querySelector(".footer");
+
+export const checkTaskNumber = () => {
+  const taskCount = todoContainer.childElementCount;
+  console.log(taskCount);
+  if (taskCount == 0) {
+    mainContainer.classList.add("hidden");
+    footerContainer.classList.add("hidden");
+  } else {
+    mainContainer.classList.remove("hidden");
+    footerContainer.classList.remove("hidden");
+  }
 };
