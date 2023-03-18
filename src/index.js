05c6cf1addf40e6e90acdf8d1c1d47bee6c788c6
Edward Rangel <ejrangel7@gmail.com>
2023-03-18 18:57:09 -0500

    ocultar/mostrar secciones de main y footer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./css/base.css";
 
-import { sayHello } from "./js/utils";
+import { sayHello, showSections, hideSections } from "./js/utils";
 
 console.log(sayHello("Hello"));
+
+const toDoItems = ["hola"];
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    if(toDoItems.length > 0) {
+        showSections();
+    } else {
+        hideSections();
+    }
+} )
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,13 @@
 export const sayHello = (text) => {
   return text;
 };
+
+export const showSections = () => {
+  document.querySelector(".main").classList.remove("hidden");
+  document.querySelector(".footer").classList.remove("hidden");
+}
+
+export const hideSections = () => {
+  document.querySelector(".main").classList.add("hidden");
+  document.querySelector(".footer").classList.add("hidden");
+}
