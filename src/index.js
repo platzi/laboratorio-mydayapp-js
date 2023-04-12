4086badc216e7064008ce7d0ef3dffa4827f7f84
Daniel <dny.velasquez@gmail.com>
2023-04-12 17:25:10 -0500

    insert new to do

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import "./css/base.css";
 
-import { sayHello } from "./js/utils";
+import { data } from './js/data.js';
 
-console.log(sayHello("Hello"));
+import { todoArray } from './js/utils'
+
+import { newTodo } from './js/utils';
+import { todoList } from './js/utils';
+
+newTodo.addEventListener('keyup', (e) => {
+    if(e.keyCode == 13) {
+        if(newTodo.value != '') {
+            const newData = new data;
+            newData.id = todoArray.length;
+            newData.title = newTodo.value.trim();
+            newData.completed = false;
+            todoArray.push(newData);
+            newTodo.value = '';
+            console.log(todoArray);
+            const todoLi = document.createElement('li');
+            const todoDiv = document.createElement('div');
+            todoDiv.classList.add('view');
+            const todoInput = document.createElement('input');
+            todoInput.classList.add('toggle');
+            todoInput.setAttribute('type', 'checkbox');
+            const todoLabel = document.createElement('label');
+            todoLabel.innerText = newData.title;
+            const todoButton = document.createElement('button');
+            todoButton.classList.add('destroy');
+            const editInput = document.createElement('input');
+            editInput.classList.add('edit');
+            editInput.setAttribute('value', newData.title);
+            todoList.appendChild(todoLi);
+            todoLi.appendChild(todoDiv);
+            todoDiv.appendChild(todoInput);
+            todoDiv.appendChild(todoLabel);
+            todoDiv.appendChild(todoButton);
+            todoLi.appendChild(editInput);
+        }
+    } 
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,6 @@
-export const sayHello = (text) => {
-  return text;
-};
+import { doc } from "prettier";
+
+export const todoArray = [];
+
+export const newTodo = document.querySelector('.new-todo');
+export const todoList = document.querySelector('.todo-list');
