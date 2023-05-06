152820712b015197c9c80bac33008b4121562f19
Juan José Gómez A <jjgomeza22@gmail.com>
2023-05-06 17:54:06 -0500

    Se agrega funcionalidad 1. Ocultar las secciones main y footer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import "./css/base.css";
+import { sayHello, todos } from "./js/utils";
 
-import { sayHello } from "./js/utils";
+// const todos = [];
+
+const main = document.querySelector(".main");
+const footer = document.querySelector(".footer");
 
 console.log(sayHello("Hello"));
+
+function showInitialInfo() {
+  if (!todos.length) {
+    main.classList.add("hidden");
+    footer.classList.add("hidden");
+  }
+}
+
+showInitialInfo();
