5decec699a6c3f7e0da88cd275872061348792aa
Nelson Ramos <nelsonalejandroramosrivera@gmail.com>
2023-05-05 21:18:05 -0400

    primera version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
 import "./css/base.css";
 
-import { sayHello } from "./js/utils";
+import { cargar, crear, completar, filtrar, eliminar } from './js/utils.js';
 
-console.log(sayHello("Hello"));
+/*#########################################################
+            CARGAR INICIO DE LA APLICACION
+#########################################################*/
+window.onload = () => {
+  if (localStorage.getItem("todo")) {
+    cargar();
+  } else {
+    localStorage.setItem("todo", JSON.stringify([]));
+  }
+}
+
+/*#########################################################
+             ESCUCHA EVENTO PRESIONAR TECLA
+#########################################################*/
+window.onkeypress = function (event, index) {
+  crear(event);
+}
+
+/*#########################################################
+             ESCUCHA EVENTO CLICK
+#########################################################*/
+window.onclick = function (event, index) {
+  switch (event.target.localName) {
+    case "input":
+      completar(event);
+      break;
+    case "a":
+      filtrar(event);
+      break;
+    case "button":
+      eliminar(event);
+      break;
+    default:
+      console.log("evento no programado");
+      break;
+  }
+}
+
+/*#########################################################
+             ESCUCHA EVENTO DOBLE CLICK
+#########################################################*/
+window.ondblclick = function (event) {
+  // console.log("double");
+}
+
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,85 @@
-export const sayHello = (text) => {
-  return text;
+const checkbox = (status) => { return status == "completed" ? 'checked' : '' }
+const limpiar = () => {
+  document.querySelector(".todo-list").innerHTML = "";
+  document.querySelector("#count").innerHTML = "";
 };
+
+/*#########################################################
+            FUNCIONES PRINCIPALES
+#########################################################*/
+export function cargar(filter = undefined) {
+  limpiar();
+  let list = JSON.parse(localStorage.getItem("todo"));
+  const subject = document.querySelector(".todo-list");
+  list.filter(e => filter != undefined ? e.status == filter : e).forEach((element, index) => {
+    subject.insertAdjacentHTML(
+      "afterbegin",
+      ` <li class="${element.status}">
+        <div class="view">
+          <input id="${index}" ${checkbox(element.status)} class="toggle" type="checkbox"/>
+           <label> ${element.title}</label>
+    <button class="destroy" id="${index}"></button>
+        </div >
+      <input class="edit" value="Learn JavaScript" />
+      </li > `
+    );
+  });
+  document.querySelector("#count").insertAdjacentHTML(
+    "afterbegin",
+    list.length
+  );
+}
+export function crear(e) {
+  if (e.keyCode === 13 && !e.shiftKey) {
+    e.preventDefault();
+    let list = JSON.parse(localStorage.getItem("todo"));
+    list.push(
+      {
+        title: e.target.value,
+        status: "pending"
+      }
+    );
+    document.querySelector(".new-todo").value = '';
+    localStorage.setItem("todo", JSON.stringify(list));
+  }
+  cargar();
+};
+export function completar(e) {
+  if (e.target.id) {
+    let list = JSON.parse(localStorage.getItem("todo"));
+    list[e.target.id].status = list[e.target.id].status == "completed" ? "pending" : "completed";
+    localStorage.setItem("todo", JSON.stringify(list));
+    cargar();
+  }
+};
+export function filtrar(e) {
+  switch (e.target.hash) {
+    case "#/":
+      cargar();
+      break;
+    case "#/pending":
+      cargar("pending");
+      break;
+    case "#/completed":
+      cargar("completed");
+      break;
+    default:
+      break;
+  }
+
+}
+export function eliminar(e) {
+  if (e.target.className == "destroy") {
+    let list = JSON.parse(localStorage.getItem("todo"));
+    list.splice(e.target.id, 1);
+    localStorage.setItem("todo", JSON.stringify(list));
+  } else {
+    limpiarCompletados();
+  }
+  cargar();
+};
+export function limpiarCompletados() {
+  let list = JSON.parse(localStorage.getItem("todo"));
+  localStorage.setItem("todo", JSON.stringify(list.filter(todo => todo.status != "completed")));
+}
+
