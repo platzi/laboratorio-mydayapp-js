bf7b10c511f7cd40c63760ea7fae483569339bda
Danilo Blas <sdann_26@hotmail.com>
2023-05-31 01:54:32 -0500

    feat: add remove and checked item & apply format

--- src/index.js
+++ src/index.js
@@ -1,56 +1,76 @@
 import "./css/base.css";
-import { addItem } from "./js/utils";
+import { addItem, removeItem, checkItem } from "./js/utils";
 
 const wrapper = document.querySelector(".todoapp-wrapper");
-const todoListItems = JSON.parse(localStorage.getItem("todoList")) ?? [];
+const todoListItems = JSON.parse(localStorage.getItem("mydayapp-js")) ?? [];
 
 const inputAddTodo = document.querySelector(".new-todo");
 const todoList = document.querySelector(".todo-list");
-const todoCount = document.querySelector(".todo-count")
+const todoCount = document.querySelector(".todo-count");
+// const clearAllButton = document.querySelector("clear-completed")
+
+/* Functions */
+const updateTodoCount = () => {
+  todoCount.innerHTML =
+    todoListItems.length !== 1
+      ? `<strong>${todoListItems.length}</strong> items left`
+      : `<strong>1</strong> item left`;
+};
 
 /* First Render */
-if(todoListItems.length === 0) {
-  const children = Array.from(wrapper.children)
-  children.forEach(child => {
-    child.classList.add('hidden')
+if (todoListItems.length === 0) {
+  const children = Array.from(wrapper.children);
+  children.forEach((child) => {
+    child.classList.add("hidden");
   });
 } else {
   const auxList = document.createDocumentFragment();
-  
-  todoListItems.forEach(item => {
+
+  todoListItems.forEach((item) => {
     const listItem = document.createElement("li");
+    listItem.id = item.id;
+    if (item.completed) listItem.classList.add("completed");
+
     listItem.innerHTML = `
-      <div class=${item.completed ? "completed" : "view"} id=${item.id}>
-        <input class="toggle" type="checkbox">
+      <div class="view">
+        <input class="toggle" type="checkbox" ${
+          item.completed ? "checked" : ""
+        }>
         <label>${item.title}</label>
         <button class="destroy"></button>
       </div>
       <input class="edit" value=${item.title}>
     `;
-    auxList.appendChild(listItem)
-  })
+    auxList.appendChild(listItem);
+  });
 
-  todoList.appendChild(auxList)
-  todoCount.children[0].textContent = todoListItems.length
+  todoList.appendChild(auxList);
+  updateTodoCount();
 }
 
+/* Event Listener */
 inputAddTodo.addEventListener("keydown", function (e) {
   const todoText = e.target.value.trim();
+  const id = Date.now().toString() + todoText;
 
   if (e.key === "Enter" && todoText.length > 0) {
-    if(todoListItems.length === 0) {
-      const children = Array.from(wrapper.children)
-      children.forEach(child => {
-        child.classList.remove('hidden')
+    if (todoListItems.length === 0) {
+      const children = Array.from(wrapper.children);
+      children.forEach((child) => {
+        child.classList.remove("hidden");
       });
     }
     const listItem = document.createElement("li");
+    listItem.id = id;
 
-    addItem({
-      id: Date.now(),
-      title: todoText,
-      completed: false,
-    }, todoListItems)
+    addItem(
+      {
+        id: id,
+        title: todoText,
+        completed: false,
+      },
+      todoListItems
+    );
 
     listItem.innerHTML = `
       <div class="view">
@@ -63,6 +83,32 @@ inputAddTodo.addEventListener("keydown", function (e) {
 
     todoList.appendChild(listItem);
     inputAddTodo.value = "";
-    todoCount.children[0].textContent = todoListItems.length
+    todoCount.children[0].textContent = todoListItems.length;
+    updateTodoCount();
+  }
+});
+
+todoList.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON" && e.target.classList.contains("destroy")) {
+    const item = e.target.parentNode.parentNode;
+
+    removeItem(item.id, todoListItems);
+    todoList.removeChild(item);
+    updateTodoCount();
+
+    if (todoListItems.length === 0) {
+      const children = Array.from(wrapper.children);
+      children.forEach((child) => {
+        child.classList.add("hidden");
+      });
+    }
+  }
+
+  if (e.target.tagName === "INPUT" && e.target.classList.contains("toggle")) {
+    const item = e.target.parentNode.parentNode;
+    checkItem(item.id, todoListItems, e.target.checked);
+    e.target.checked
+      ? item.classList.add("completed")
+      : item.classList.remove("completed");
   }
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,27 @@
 export const addItem = (item, todoList) => {
   todoList.push(item);
-  
-  localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  localStorage.setItem("mydayapp-js", JSON.stringify(todoList));
 };
 
-export const removeItem = () => { }
+export const removeItem = (id, todoList) => {
+  const index = todoList.findIndex((todo) => {
+    console.log(todo.id, id);
+    return todo.id === id;
+  });
+  todoList.splice(index, 1);
+  localStorage.setItem("mydayapp-js", JSON.stringify(todoList));
+};
 
-export const editItem = () => { }
+export const checkItem = (id, todoList, value) => {
+  const index = todoList.findIndex(
+    (todo) => todo.id.toString() === id.toString()
+  );
+
+  todoList[index].completed = value;
+
+  console.log(todoList);
+  localStorage.setItem("mydayapp-js", JSON.stringify(todoList));
+};
+
+export const editItem = () => {};
