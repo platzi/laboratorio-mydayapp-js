411d254f4772969cc8f599ebaeb8dcedc3cd0163
Danilo Blas <sdann_26@hotmail.com>
2023-05-31 23:11:14 -0500

    refactor: create temmplate for listItem

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ import {
   clearCompletedItems,
 } from "./js/utils";
 
-const wrapper = document.querySelector(".todoapp-wrapper");
 let todoListItems = JSON.parse(localStorage.getItem("mydayapp-js")) ?? [];
 
+/* Elements */
+const wrapper = document.querySelector(".todoapp-wrapper");
 const inputAddTodo = document.querySelector(".new-todo");
 const todoList = document.querySelector(".todo-list");
 const todoCount = document.querySelector(".todo-count");
 const clearAllButton = document.querySelector(".clear-completed");
 
 /* Functions */
-const updateTodoCount = () => {
+const updateTodoCount = (todoListItems) => {
   todoCount.innerHTML =
     todoListItems.length !== 1
       ? `<strong>${todoListItems.length}</strong> items left`
@@ -31,6 +32,25 @@ const isListItemEmpy = () => {
   }
 };
 
+const createListItemTemplate = ({ id, text, completed }) => {
+  const listItem = document.createElement("li");
+  listItem.id = id;
+  if(completed) listItem.classList.add("completed")
+
+  listItem.innerHTML = `
+    <div class="view">
+      <input class="toggle" type="checkbox" ${
+        completed ? "checked" : ""
+      }>
+      <label>${text}</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value=${text}>
+  `;
+
+  return listItem
+}
+
 /* First Render */
 if (todoListItems.length === 0) {
   isListItemEmpy();
@@ -38,25 +58,12 @@ if (todoListItems.length === 0) {
   const auxList = document.createDocumentFragment();
 
   todoListItems.forEach((item) => {
-    const listItem = document.createElement("li");
-    listItem.id = item.id;
-    if (item.completed) listItem.classList.add("completed");
-
-    listItem.innerHTML = `
-      <div class="view">
-        <input class="toggle" type="checkbox" ${
-          item.completed ? "checked" : ""
-        }>
-        <label>${item.title}</label>
-        <button class="destroy"></button>
-      </div>
-      <input class="edit" value=${item.title}>
-    `;
+    const listItem = createListItemTemplate({ id: item.id, text: item.title, completed: item.completed})
     auxList.appendChild(listItem);
   });
 
   todoList.appendChild(auxList);
-  updateTodoCount();
+  updateTodoCount(todoListItems);
 }
 
 /* Event Listener */
@@ -71,8 +78,6 @@ inputAddTodo.addEventListener("keydown", function (e) {
         child.classList.remove("hidden");
       });
     }
-    const listItem = document.createElement("li");
-    listItem.id = id;
 
     addItem(
       {
@@ -83,19 +88,12 @@ inputAddTodo.addEventListener("keydown", function (e) {
       todoListItems
     );
 
-    listItem.innerHTML = `
-      <div class="view">
-        <input class="toggle" type="checkbox">
-        <label>${todoText}</label>
-        <button class="destroy"></button>
-      </div>
-      <input class="edit" value=${todoText}>
-    `;
+    const listItem = createListItemTemplate({ id: id, text: todoText, completed: false})
 
     todoList.appendChild(listItem);
     inputAddTodo.value = "";
     todoCount.children[0].textContent = todoListItems.length;
-    updateTodoCount();
+    updateTodoCount(todoListItems);
   }
 });
 
@@ -105,12 +103,13 @@ todoList.addEventListener("click", function (e) {
 
     removeItem(item.id, todoListItems);
     todoList.removeChild(item);
-    updateTodoCount();
+    updateTodoCount(todoListItems);
     isListItemEmpy();
   }
 
   if (e.target.tagName === "INPUT" && e.target.classList.contains("toggle")) {
     const item = e.target.parentNode.parentNode;
+
     checkItem(item.id, todoListItems, e.target.checked);
     e.target.checked
       ? item.classList.add("completed")
@@ -128,7 +127,7 @@ clearAllButton.addEventListener("click", function () {
 
     clearCompletedItems(todoListItems);
     todoListItems = JSON.parse(localStorage.getItem("mydayapp-js"));
-    updateTodoCount();
+    updateTodoCount(todoListItems);
     isListItemEmpy();
   }
 });
