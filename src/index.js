330aaed9a13729e5d6b36d28be7007967dc4a800
Joshua Daniel <joshua_dup@hotmail.com>
2023-06-09 13:26:39 -0500

    inicio creando la funcion que guardara toda la logica del programa, defino variables que referencian elementos HTML y creo evento keydown

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./css/base.css";
 
-import { sayHello } from "./js/utils";
+import { init } from "./js/utils";
 
-console.log(sayHello("Hello"));
+// console.log(sayHello("Hello"));
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,31 @@
-export const sayHello = (text) => {
-  return text;
-};
+export const init = document.addEventListener("DOMContentLoaded", () => {
+  const newTaskInput = document.querySelector(".new-todo");
+  const taskList = document.querySelector(".todo-list");
+  const taskCount = document.querySelector(".todo-count");
+  const clearCompletedBtn = document.querySelector(".clear-completed");
+
+  let task = [];
+
+  // Añado un evento keydown al input
+  newTaskInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const taskTitle = newTaskInput.value.trim();
+      if (taskTitle !== "") {
+        const newTask = {
+          id: generateId(),
+          title: taskTitle,
+          completed: false,
+        };
+        task.push(newTask);
+        newTaskInput.value = "";
+        renderTasks();
+        saveTasks();
+      }
+    }
+  });
+
+  // Genero id
+  function generateId() {
+    return Math.random().toString(36).substring(2, 9);
+  }
+});
