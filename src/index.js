a04a66e0fcc9dbe93a3d00316a56a481e6b9f3fd
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-08-28 19:20:25 -0600

    Ahora se oculta el main section y el footer cuando no hay tareas

--- src/index.js
+++ src/index.js
@@ -3,3 +3,27 @@ import "./css/base.css";
 import { sayHello } from "./js/utils";
 
 console.log(sayHello("Hello"));
+
+const mainSection = document.getElementById("main");
+const footer = document.getElementById("footer");
+
+function GetTasks() {
+  const TaskList = JSON.parse(localStorage.getItem("Task-List"));
+
+  if (TaskList) {
+    return TaskList;
+  } else {
+    return {};
+  }
+}
+
+function RenderTasks() {
+  const TaskList = GetTasks();
+
+  if (Object.keys(TaskList).length === 0) {
+    mainSection.style.display = "none";
+    footer.style.display = "none";
+  }
+}
+
+RenderTasks();
