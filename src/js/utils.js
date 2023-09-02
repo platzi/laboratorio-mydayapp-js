26888f1b52ba0e4e65cd2232566338d64f9e6e33
Luis Valladares <ls.valladares7@gmail.com>
2023-09-02 12:55:56 -0600

    feat(4.1): actualizacion de texto para las tareas

--- src/js/utils.js
+++ src/js/utils.js
@@ -18,9 +18,23 @@ export const addTodoController = (text) => {
   $todoStatusCheckbox.addEventListener("change", (e) => updateTodoStatus(newTodo.id, e.target.checked));
   $todoLabel.addEventListener("dblclick", () => activateTodoEditMode(newTodo.id));
   $todoEditInput.addEventListener("focusout", () => disableTodoEditMode(newTodo.id));
+  $todoEditInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") updateTodoText(newTodo.id, e.target.value);
+  })
   // Agrega nodo al HTML
   const $todoList = document.querySelector(".todo-list");
   $todoList.appendChild($todoElement.firstElementChild);
+}
+
+export const updateTodoText = (todoId, text) => {
+  // Actualiza TODO en el estado global
+  updateTodo(todoId, { text });
+  // Actualiza TODO en el HTML y deshabilita modo edicion
+  const $todoContainer =  document.querySelector(`li[data-todo-id="${todoId}"]`);
+  const $todoLabel =  $todoContainer.querySelector(`label`);
+  const $todoEditInput =  $todoContainer.querySelector(`input.edit`);
+  $todoLabel.innerHTML = text;
+  $todoEditInput.blur();
 }
 
 export const updateTodoStatus = (todoId, isCompleted) => {
