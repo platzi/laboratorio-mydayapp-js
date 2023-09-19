2fbb5c422e7089aea547b935ab9ef57c10967af5
julian-uribe47 <juribedio@hotmail.com>
2023-09-18 20:33:07 -0500

    Ocultar secciones Main y Footer si no existen tareas pendientes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import "./css/base.css";
 
-import { sayHello } from "./js/utils";
+// import { sayHello } from "./js/utils";
+//#1 Ocultar las secciones main y footer
+const main = document.querySelector("#main");
+const footer = document.querySelector("#footer");
 
-console.log(sayHello("Hello"));
+const contenedorTareas = document.getElementsByClassName("todo-list");
+const tareas = contenedorTareas[0].children;
+
+if (tareas.length == 0) {
+  main.classList.add("inactive");
+  footer.classList.add("inactive");
+} else {
+  main.classList.remove("inactive");
+  footer.classList.remove("inactive");
+}
