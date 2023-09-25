69e408b257149dd7865b7132dbcbefeb90dfd338
julian-uribe47 <juribedio@hotmail.com>
2023-09-24 20:15:43 -0500

    Guardar tarea y validar los valores del input

--- src/js/utils.js
+++ src/js/utils.js
@@ -30,13 +30,29 @@ export const listernerCheckboxComplete = () => {
 export const listennerDobleClick = () => {
   const editTareaList = document.querySelectorAll(".nombre-tarea");
   editTareaList.forEach((tarea) => {
+    const contenedorTarea = tarea.parentNode.parentNode;
+    const edicion = contenedorTarea.querySelector(".edit");
+    const valorOriginal = edicion.value;
     tarea.addEventListener("dblclick", () => {
-      const contenedorTarea = tarea.parentNode.parentNode;
       contenedorTarea.classList.toggle("editing");
-      const edicion = contenedorTarea.querySelector(".edit");
       const end = edicion.value.length;
       edicion.setSelectionRange(end, end);
       edicion.focus();
+      edicion.addEventListener("keyup", (event) => {
+        //Guardar edición al presionar "Enter"//
+        if (event.key === "Enter") {
+          let tareaEditada = edicion.value;
+          tareaEditada = tareaEditada.trim();
+          if (tareaEditada.length > 0) {
+            tarea.innerText = tareaEditada;
+            contenedorTarea.classList.remove("editing");
+          }
+        } else if (event.key === "Escape") {
+          //al presionar "Escape" sale del modo edición y se retorna el valor original//
+          edicion.value = valorOriginal;
+          contenedorTarea.classList.remove("editing");
+        }
+      });
     });
   });
 };
