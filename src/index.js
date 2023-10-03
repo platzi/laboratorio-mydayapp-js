8ee0341df3995023a23de44c69bc83a0537418b9
CH19 <chcc190505@gmail.com>
2023-10-03 19:30:59 -0400

    feath: se añadio la creacion de notas en la aplicacion y la interaccion basica, bug: se tiene un error en la interaccion de las notas con el boton se tratara de arreglar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./css/base.css";
 
 import { sayHello } from "./js/utils";
+import { main } from "./js/utils";
 
+main();
 console.log(sayHello("Hello"));
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,54 @@
+const list = document.querySelector('.todo-list');
 export const sayHello = (text) => {
+  console.log(list.childElementCount);
   return text;
+
 };
+// Funcion que crea el todo
+function createTodo(){
+  document.querySelector('.new-todo').addEventListener('keydown', (event)=>{
+    // event.preventDefault();
+    if(event.key == 'Enter' && event.target.value != ''){
+      const word = event.target.value.trim();
+      setTimeout(newTodo(word, false), 0)
+      event.target.value = '';
+      console.log('object');
+
+    }
+    // console.log(event.key);
+  })
+}
+function newTodo(tarea, status){
+const li = document.createElement('li');
+  const todo = `
+  <li class="${status ? 'completed' : ''}">
+  <div class="view">
+    <input class="toggle" type="checkbox"  />
+    <label>${tarea}</label>
+    <button class="destroy"></button>
+  </div>
+  <input class="edit" value="Learn JavaScript"  />
+</li>
+  `
+
+  li.innerHTML = todo;
+  document.querySelector('label').addEventListener('click', ()=>{
+    alert('hola mundo');
+  })
+  list.appendChild(li)
+}
+function withoutTodo(){
+  if(list.childElementCount < 1){
+    document.querySelector('.main').remove();
+    document.querySelector('.footer').remove();
+  }
+}
+
+export function main(){
+  createTodo()
+  withoutTodo();
+
+  // newTodo('Escribir poema', true);
+  // newTodo('Hablar frances', false);
+
+}
