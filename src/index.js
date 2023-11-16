95af8868c2ff0825ea94c98a5f3fdfe4a33bd957
José Peralta <joseperalta2910@gmail.com>
2023-11-16 12:23:38 -0400

    Added functionality of adding todos to localstorage. Added functionaility of deleting todos to localstorage. Added functionality of fetching todos from localstorage. Added functionality of toggling todos completed status on localstorage.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import "./css/base.css";
 
-import { sayHello } from "./js/utils";
+async function fetchTodos(API) {
+  let todos = localStorage.getItem(API);
+  if(todos !== null) {
+    return JSON.parse(todos);
+  }
+  localStorage.setItem(API, '[]');
+  return [];
+}
 
-console.log(sayHello("Hello"));
+async function addTodo(API, todo) {
+  let todos = localStorage.getItem(API);
+  todos = JSON.parse(todos);
+  todos.push(todo);
+  localStorage.setItem(API, JSON.stringify(todos));
+}
+
+async function deleteTodo(API, todoid) {
+  let todos = localStorage.getItem(API);
+  todos = JSON.parse(todos);
+  let todoIndex = todos.findIndex((todo)=>todo.id == todoid);
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+    localStorage.setItem(API, JSON.stringify(todos));
+  }
+}
+
+async function toggleCompleteTodo(API, todoid) {
+  let todos = localStorage.getItem(API);
+  todos = JSON.parse(todos);
+  let todoIndex = todos.findIndex((todo)=>todo.id === todoid);
+  if (todoIndex !== -1) {
+    todos[todoIndex].completed = !todos[todoIndex].completed
+    localStorage.setItem(API, JSON.stringify(todos));
+  }
+}
+
+async function main() {
+  let todos = await fetchTodos(API);
+}
+
+const API = 'mydayapp-js';
+
+main();
