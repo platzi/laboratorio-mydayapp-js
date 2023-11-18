35611e3888253081cfe698264260b3fd08f898a0
José Peralta <joseperalta2910@gmail.com>
2023-11-18 19:10:06 -0400

    Code refactorized

--- src/index.js
+++ src/index.js
@@ -1,122 +1,99 @@
 import "./css/base.css";
 import { fetch, add, edit, toggleComplete, remove } from "./js/storage";
 
-function renderTodo(todo, todos) {
-  let li_todo = document.createElement("li");
-  let todo_view = document.createElement("div");
-  let todo_input = document.createElement("input");
-  let todo_label = document.createElement("label"); 
-  let todo_button = document.createElement("button");
-  let edit_todo_input = document.createElement("input");
+function render(todos) {
 
-  todo_input.type = "checkbox";
-  todo_input.classList.add("toggle");
-
-  todo_label.innerText = todo.title;
-
-  if(todo.completed) {
-    li_todo.classList.add("completed");
-    todo_input.checked = true;
-  };
-
-  todo_button.classList.add("destroy");
-
-  edit_todo_input.classList.add("edit");
-
-  todo_view.classList.add("view");
-
-  todo_view.append(todo_input, todo_label, todo_button);
-  li_todo.append(todo_view, edit_todo_input);
-
-  document.querySelector(".todo-list").append(li_todo);
-
-  todo_input.addEventListener("click", ()=>{
-    li_todo.classList.toggle("completed");
-    toggleComplete(localStorageKey, todo.id);
-    if(todo.completed === false) updateCounter(-1);
-    else updateCounter(1);
-    todo.completed = !todo.completed;
-
-    let todoIndex = todos.findIndex((todo)=>todo.id === todo.id);
-    todo[todoIndex].completed = !todo[todoIndex].completed;
-  });
-
-  todo_button.addEventListener("click", async ()=>{
-    await remove(localStorageKey, todo.id);
-    document.querySelector(".todo-list").removeChild(li_todo);
-    if(todo.completed === false) updateCounter(-1);
-
-    let todoIndex = todos.findIndex((todo)=>todo.id === todo.id);
-    todos.splice(todoIndex, 1);
-  });
-
-  todo_label.addEventListener("click", ()=>{
-    edit_todo_input.style.display = "block";
-    edit_todo_input.value = todo_label.innerText;
-    edit_todo_input.focus();
-    li_todo.classList.add("editing");
-  });
-
-  edit_todo_input.addEventListener("keydown", (e)=>{
-    if(e.key === "Enter") {
-      if(e.target.value.trim() !== "") {
-        let todoIndex = todos.findIndex((todo)=>todo.id === todo.id);
-        todo[todoIndex].title = e.target.value.trim();
-
-        todo_label.innerText = e.target.value.trim();
-        edit_todo_input.style.display = "none";
-        edit(localStorageKey, todo.id, e.target.value.trim());
-        li_todo.classList.remove("editing");
-    };
-    }
-    else if(e.key === "Escape") {
-      edit_todo_input.style.display = "none";
-      li_todo.classList.remove("editing");
-    }
-  })
-}
-
-async function createNewTodo(e, todos) {
-  if(e.target.value.trim() !== ""){
-    let id = todos.length > 0 ? todos[todos.length-1].id + 1 : 0;
-
-    let todo = {
-      id, 
-      title:e.target.value.trim(), 
-      completed: false
-    };
-
-    renderTodo(todo, todos);
-    await add(localStorageKey, todo);
-    updateCounter(1);
-    e.target.value = "";
-
-    todos.push(todo);
+  let list = document.querySelector('.todo-list');
+  while(list.hasChildNodes()) {
+    list.removeChild(list.lastChild);
   }
+
+  todos.forEach((todo)=>{
+    let li_todo = document.createElement("li");
+    let todo_view = document.createElement("div");
+    let todo_input = document.createElement("input");
+    let todo_label = document.createElement("label"); 
+    let todo_button = document.createElement("button");
+    let edit_todo_input = document.createElement("input");
+  
+    todo_input.type = "checkbox";
+    todo_input.classList.add("toggle");
+  
+    todo_label.innerText = todo.title;
+  
+    if(todo.completed) {
+      li_todo.classList.add("completed");
+      todo_input.checked = true;
+    };
+  
+    todo_button.classList.add("destroy");
+  
+    edit_todo_input.classList.add("edit");
+  
+    todo_view.classList.add("view");
+  
+    todo_view.append(todo_input, todo_label, todo_button);
+    li_todo.append(todo_view, edit_todo_input);
+  
+  
+    todo_input.addEventListener("click", ()=>toggleComplete(localStorageKey, todo.id));
+  
+    todo_button.addEventListener("click", ()=> remove(localStorageKey, todo.id));
+  
+    edit_todo_input.addEventListener("keydown", (e)=>{
+      if(e.key === "Enter") {
+        if(e.target.value.trim() !== "") {
+          edit(localStorageKey, todo.id, e.target.value.trim());
+      };
+      }
+      else if(e.key === "Escape") {
+        edit_todo_input.style.display = "none";
+        li_todo.classList.remove("editing");
+      }
+    });
+
+    todo_label.addEventListener("click", ()=>{
+      edit_todo_input.style.display = "block";
+      edit_todo_input.value = todo_label.innerText;
+      edit_todo_input.focus();
+      li_todo.classList.add("editing");
+    });
+  
+    list.appendChild(li_todo);
+    
+  });
+  updateCounter(todos.filter((todo)=>!todo.completed).length);
 }
 
-function updateCounter(num) {
-  counter += num;
-  if(counter === 1) document.querySelector(".todo-count").innerHTML = `<strong>${counter}</strong> item left`;
-  else document.querySelector(".todo-count").innerHTML = `<strong>${counter}</strong> items left`;
+function updateCounter(count) {
+  if(count === 1) document.querySelector(".todo-count").innerHTML = `<strong>${count}</strong> item left`;
+  else document.querySelector(".todo-count").innerHTML = `<strong>${count}</strong> items left`;
 }
 
-async function main() {
-  let todos = await fetch(localStorageKey);
+function main() {
+  let todos = fetch(localStorageKey);
+  let createTodoInput = document.querySelector(".new-todo");
 
-  todos.forEach(todo => {
-    renderTodo(todo, todos);
-    if(todo.completed === false) updateCounter(1);
+  render(todos);
+
+  window.addEventListener("storage", () => {
+    todos = fetch(localStorageKey);
+    render(todos);
   });
 
-  document.querySelector(".new-todo").addEventListener("keydown", async (e)=>{
-    if(e.key === 'Enter'){
-      await createNewTodo(e, todos);
-    }
-  });
+  createTodoInput.addEventListener("keydown", (e)=>{
+      if(e.key === "Enter") {
+          if(e.target.value.trim() !== "") { 
+          let id;
+          todos = fetch(localStorageKey);
+          id = todos.length > 0 ? todos[todos.length-1].id + 1 : 0;
+          add(localStorageKey, {id, title: e.target.value.trim(), completed: false });
+        }
+      }
+    });
 }
 
 const localStorageKey = "mydayapp-js";
-let counter = 0;
+
 
 main();
--- src/js/storage.js
+++ src/js/storage.js
@@ -1,4 +1,4 @@
-async function fetch(localStorageKey) {
+function fetch(localStorageKey) {
   let todos = localStorage.getItem(localStorageKey);
   if(todos !== null) {
     return JSON.parse(todos);
@@ -7,40 +7,44 @@ async function fetch(localStorageKey) {
   return [];
 }
 
-async function add(localStorageKey, todo) {
+function add(localStorageKey, todo) {
   let todos = localStorage.getItem(localStorageKey);
   todos = JSON.parse(todos);
   todos.push(todo);
   localStorage.setItem(localStorageKey, JSON.stringify(todos));
+  window.dispatchEvent(new Event('storage'));
 }
 
-async function remove(localStorageKey, todoid) {
+function remove(localStorageKey, todoid) {
   let todos = localStorage.getItem(localStorageKey);
   todos = JSON.parse(todos);
   let todoIndex = todos.findIndex((todo)=>todo.id == todoid);
   if (todoIndex !== -1) {
     todos.splice(todoIndex, 1);
     localStorage.setItem(localStorageKey, JSON.stringify(todos));
+    window.dispatchEvent(new Event('storage'));
   }
 }
 
-async function toggleComplete(localStorageKey, todoid) {
+function toggleComplete(localStorageKey, todoid) {
   let todos = localStorage.getItem(localStorageKey);
   todos = JSON.parse(todos);
   let todoIndex = todos.findIndex((todo)=>todo.id === todoid);
   if (todoIndex !== -1) {
     todos[todoIndex].completed = !todos[todoIndex].completed;
     localStorage.setItem(localStorageKey, JSON.stringify(todos));
+    window.dispatchEvent(new Event('storage'));
   }
 }
 
-async function edit(localStorageKey, todoid, text) {
+function edit(localStorageKey, todoid, text) {
   let todos = localStorage.getItem(localStorageKey);
   todos = JSON.parse(todos);
   let todoIndex = todos.findIndex((todo)=>todo.id === todoid);
   if (todoIndex !== -1) {
     todos[todoIndex].title = text;
     localStorage.setItem(localStorageKey, JSON.stringify(todos));
+    window.dispatchEvent(new Event('storage'));
   }
 }
 
