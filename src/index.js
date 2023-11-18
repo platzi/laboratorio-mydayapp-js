110646aec76f5439e401290c8a2766ad4dc97147
José Peralta <joseperalta2910@gmail.com>
2023-11-17 20:01:46 -0400

    Added functionality of render todos. Added functionality of create new todos. Added functionality of delete todos. Added functionality of complete and uncomplete todos.

--- src/index.js
+++ src/index.js
@@ -36,8 +36,60 @@ async function toggleCompleteTodo(API, todoid) {
   }
 }
 
+function renderTodo(todo) {
+  let li_todo = document.createElement('li');
+  let todo_view = document.createElement('div');
+  let todo_input = document.createElement('input');
+  let todo_label = document.createElement('label');
+  let todo_button = document.createElement('button');
+
+  todo_input.type = 'checkbox';
+  todo_input.classList.add('toggle');
+
+  todo_label.innerText = todo.title;
+
+  if(todo.completed) {
+    li_todo.classList.add('completed');
+    todo_input.checked = true;
+  };
+
+  todo_button.classList.add('destroy');
+
+  todo_view.append(todo_input, todo_label, todo_button);
+  li_todo.append(todo_view);
+
+  document.querySelector('.todo-list').append(li_todo);
+
+  todo_input.addEventListener('click', ()=>{
+    li_todo.classList.toggle('completed');
+    toggleCompleteTodo(API, todo.id);
+  });
+
+  todo_button.addEventListener('click', ()=>{
+    document.querySelector('.todo-list').removeChild(li_todo);
+    deleteTodo(API, todo.id);
+  })
+}
+
 async function main() {
   let todos = await fetchTodos(API);
+
+  todos.forEach(todo => {
+    renderTodo(todo);
+  });
+
+  document.querySelector('.new-todo').addEventListener('keydown', (e)=>{
+    if(e.key === 'Enter'){
+      if(e.target.value.trim() !== ''){
+        let todo = {id:todos[todos.length-1].id + 1, title:e.target.value, completed: false};
+        renderTodo(todo);
+        addTodo(API, todo);
+        e.target.value = '';
+      }
+    }
+  });
+
+  console.log();
 }
 
 const API = 'mydayapp-js';
