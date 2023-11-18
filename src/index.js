3150d048249207543055be1781cebae7c40d651a
José Peralta <joseperalta2910@gmail.com>
2023-11-18 12:19:20 -0400

    Counter functionality added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./css/base.css";
 import { fetchTodos, addTodo, editTodo, toggleCompleteTodo, deleteTodo } from "./js/utils";
 
-function renderTodo(todo) {
+function renderTodo(todo, counter) {
   let li_todo = document.createElement("li");
   let todo_view = document.createElement("div");
   let todo_input = document.createElement("input");
-  let todo_label = document.createElement("label");
+  let todo_label = document.createElement("label"); 
   let todo_button = document.createElement("button");
   let edit_todo_input = document.createElement("input");
 
@@ -35,9 +35,10 @@ function renderTodo(todo) {
     toggleCompleteTodo(API, todo.id);
   });
 
-  todo_button.addEventListener("click", ()=>{
+  todo_button.addEventListener("click", async ()=>{
+    await deleteTodo(API, todo.id);
     document.querySelector(".todo-list").removeChild(li_todo);
-    deleteTodo(API, todo.id);
+    updateCounter(-1, counter);
   });
 
   todo_label.addEventListener("click", ()=>{
@@ -63,32 +64,46 @@ function renderTodo(todo) {
   })
 }
 
+async function createNewTodo(e, todos) {
+  if(e.target.value.trim() !== ""){
+    let id = todos.length > 0 ? todos[todos.length-1].id + 1 : 0;
+
+    let todo = {
+      id, 
+      title:e.target.value.trim(), 
+      completed: false
+    };
+
+    renderTodo(todo);
+    await addTodo(API, todo);
+    e.target.value = "";
+  }
+}
+
+function updateCounter(num) {
+  counter += num;
+  if(counter === 1) document.querySelector(".todo-count").innerText = `${counter} item left`;
+  else document.querySelector(".todo-count").innerText = `${counter} items left`
+}
+
 async function main() {
   let todos = await fetchTodos(API);
+  counter = todos.length;
 
   todos.forEach(todo => {
     renderTodo(todo);
   });
 
   document.querySelector(".new-todo").addEventListener("keydown", async (e)=>{
-    if(e.key === "Enter"){
-      if(e.target.value.trim() !== ""){
-        let id = todos.length > 0 ? todos[todos.length-1].id + 1 : 0;
-
-        let todo = {
-          id, 
-          title:e.target.value.trim(), 
-          completed: false
-        };
-
-        renderTodo(todo);
-        await addTodo(API, todo);
-        e.target.value = "";
-      }
+    if(e.key === 'Enter'){
+      await createNewTodo(e, todos);
+      updateCounter(1);
     }
   });
+  updateCounter(0)
 }
 
 const API = "mydayapp-js";
+let counter;
 
 main();
