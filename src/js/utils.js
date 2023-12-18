f14c346c133a3dff2f754a1474d6fe60d19fbb8c
immprada <im.mprada@gmail.com>
2023-12-17 20:26:30 -0500

    add funtions to intialize events liseteners, in roder to act when enter, and load routes handling

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,23 @@
-export const sayHello = (text) => {
-  return text;
-};
+import { listenHashChange } from "./routes/todos";
+
+const INPUT_TODO_TEXT_BOX = "new_todo_description";
+
+export const addEventToToDoInput = (toDos) => {
+  const todoInput = document.getElementById(INPUT_TODO_TEXT_BOX);
+  if(!todoInput) return;
+
+  todoInput.addEventListener('keydown', (event) => handleNewToDoInput(event, toDos));
+}
+
+export const addHashListenerToDocument = (ToDos) => {
+  window.addEventListener('hashchange', () => listenHashChange(ToDos));
+}
+
+const handleNewToDoInput = (event, toDos) => {
+  const title = event.target.value;
+  if (event.key != 'Enter') return;
+  if (title == '') return;
+  
+  toDos.addTodo(title);
+  console.log(title);
+}
