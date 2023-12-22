fd76f55c763ffebc98db73e39a6a553493214480
haroldZS <haroldzs1024@gmail.com>
2023-12-22 11:22:43 -0400

    feat(tasks): Implement functionality for creating new tasks and persistent rendering
    This commit introduces the ability to create new tasks within the
    application. Additionally, it implements persistent rendering of tasks.
    A nodes file has been created to store querySelectors in an object for
    improved DOM element management.
    
    BREAKING CHANGE: A significant change has been made in the node
    management structure, potentially impacting existing functionalities
    reliant on these nodes.
    
    Completed: #feature-2, #feature-7

--- src/index.js
+++ src/index.js
@@ -1,19 +1,79 @@
 import "./css/base.css";
 
 import { TaskManager } from "./js/localStorage";
+import { nodes } from "./js/nodes";
 
 const taskManager = new TaskManager();
 
-taskManager.addTask({ id: "123", title: "New Task", completed: true });
-taskManager.addTask({ id: "124", title: "New Task 1", completed: true });
-taskManager.addTask({ id: "125", title: "New Task 2", completed: true });
-taskManager.addTask({ id: "126", title: "New Task 3", completed: false });
-taskManager.addTask({ id: "129", title: "New Task 4", completed: false });
-taskManager.addTask({ id: "130", title: "New Task 5", completed: false });
-taskManager.deleteTask("123");
-taskManager.deleteTask("124");
-taskManager.deleteTask("125");
-taskManager.updateTask("129", "Updated");
-taskManager.updateTask("130", "New");
-taskManager.updateTask("130", "Another text");
-console.log(taskManager.getTasks());
+// taskManager.addTask({ id: "123", title: "New Task", completed: true });
+// taskManager.addTask({ id: "124", title: "New Task 1", completed: true });
+// taskManager.addTask({ id: "125", title: "New Task 2", completed: true });
+// taskManager.addTask({ id: "126", title: "New Task 3", completed: false });
+// taskManager.addTask({ id: "129", title: "New Task 4", completed: false });
+// taskManager.addTask({ id: "130", title: "New Task 5", completed: false });
+// taskManager.deleteTask("123");
+// taskManager.deleteTask("124");
+// taskManager.deleteTask("125");
+// taskManager.updateTask("129", "Updated");
+// taskManager.updateTask("130", "New");
+// taskManager.updateTask("130", "Another text");
+// console.log(taskManager.getTasks());
+
+function addNewTask(event) {
+  const taskTitle = event.target.value.trim();
+
+  if (taskTitle !== "") {
+    const randomId = Math.random().toString(36).substr(2, 5);
+    taskManager.addTask({
+      id: randomId,
+      title: taskTitle,
+      completed: false,
+    });
+  }
+}
+
+function createTaskNode({ title, completed }) {
+  const listItem = document.createElement("li");
+  const taskDiv = document.createElement("div");
+  const taskInput = document.createElement("input");
+  const taskLabel = document.createElement("label");
+  const taskButton = document.createElement("button");
+  const editTaskInput = document.createElement("input");
+
+  taskDiv.classList.add("view");
+  taskInput.type = "checkbox";
+  taskInput.classList.add("toggle");
+  taskLabel.textContent = title;
+  taskButton.classList.add("destroy");
+  editTaskInput.classList.add("edit");
+  editTaskInput.setAttribute("value", title);
+
+  if (completed) {
+    listItem.classList.add("completed");
+    taskInput.checked = true;
+  }
+
+  taskDiv.append(taskInput, taskLabel, taskButton);
+  listItem.append(taskDiv, editTaskInput);
+
+  return listItem;
+}
+
+function renderTasks() {
+  nodes.taskList.innerHTML = "";
+
+  taskManager.getTasks().forEach((task) => {
+    const taskItem = createTaskNode(task);
+    nodes.taskList.appendChild(taskItem);
+  });
+}
+
+nodes.newTaskInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addNewTask(event);
+    renderTasks();
+    nodes.newTaskInput.value = "";
+  }
+});
+
+renderTasks();
---
+++ src/js/nodes.js
@@ -0,0 +1,5 @@
+export const nodes = {
+  newTaskInput: document.querySelector(".new-todo"),
+  taskList: document.querySelector(".todo-list"),
+  footer: document.querySelector(".footer"),
+};
