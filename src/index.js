736696491c9870a27fff3da92d84171c0195cd58
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-12-27 02:00:44 -0300

    feat: :sparkles: Se creó el modulo para ocultar main y footer
    Según si hay o no elementos en la todo list .main y .footer se ocultarán o mostrarán.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 // @ts-check
 import './css/base.css';
 
-import { sayHello } from './js/utils';
+/**
+ * Check if .todo-list class is empty.
+ * @returns {boolean} If there are elements return true.
+ */
+function isTodoListEmpty() {
+	const TODO_CONTAINER = /** @type {HTMLUListElement} */ (
+		document.querySelector('.todo-list')
+	);
 
-// eslint-disable-next-line no-console
-console.log(sayHello('Hello'));
+	return TODO_CONTAINER.children.length === 0;
+}
+
+/**
+ * Hides or shows .main and .footer if there are not or are elements in todo list as appropriate.
+ * @returns {void}
+ */
+function hiddenShowMainAndFooter() {
+	const IS_EMPTY = isTodoListEmpty();
+	const MAIN_CONTAINER = /** @type {HTMLElement} */ (
+		document.querySelector('.main')
+	);
+	const FOOTER_CONTAINER = /** @type {HTMLElement} */ (
+		document.querySelector('.footer')
+	);
+
+	MAIN_CONTAINER.style.display = IS_EMPTY ? 'none' : 'block';
+	FOOTER_CONTAINER.style.display = IS_EMPTY ? 'none' : 'block';
+}
+
+hiddenShowMainAndFooter();
