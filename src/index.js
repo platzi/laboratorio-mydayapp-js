7a3c2994bd2f0d8e0fcfcf8f415b1c19969f2f04
Diego Huaccha Diestra <dhuacchadiestra@gmail.com>
2024-02-06 01:14:38 -0300

    feat: :sparkles: implement task addition functionality
    Enable the ability to add a new task to the to-do list. When a task is entered in the task addition input and 'Enter' is pressed, a new task is created and added to the to-do list array.

--- src/index.js
+++ src/index.js
@@ -11,3 +11,16 @@ if (todoList.length === 0) {
     mainSection.style.display = "none"
     footerSection.style.display = "none"
 }
+
+const newTodoInput = document.querySelector(".new-todo")
+
+newTodoInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13 && !e.shiftKey) {
+        if (!!newTodoInput.value) {
+            todoList.push({ id: Symbol(Math.floor(Math.random() * 1000)), title: newTodoInput.value.trim(), isCompleted: false})
+            newTodoInput.value = ""
+            console.log(todoList)
+        }
+    }
+})
+
--- src/js/todoList.js
+++ src/js/todoList.js
@@ -1,16 +1,16 @@
 export const todoList = [
     {
-        id: 123,
+        id: Symbol(123),
         title: "Learn JavaScript",
         isCompleted: true
     },
     {
-        id: 456,
+        id: Symbol(456),
         title: "Buy a unicorn",
         isCompleted: false
     },
     {
-        id: 789,
+        id: Symbol(789),
         title: "Make dishes",
         isCompleted: false
     },
