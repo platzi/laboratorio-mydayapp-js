9e1a8e9211c23337d2cc1d70fdfa686375b1a9c1
Alexis Liao <Alexisliao10@hotmail.com>
2024-02-24 23:57:05 -0500

    feat: hide or unhide main and footer
    Hide elements from DOM when there's no task and unhide when task are created.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import "./css/base.css";
+import * as node from "./js/nodes.js";
 
-import { sayHello } from "./js/utils";
+// main and footer should not be displayed when there's no todos
+function checkTodoTask(element) {
+  const childrenArray = Array.from(element.children);
+  if (childrenArray.length > 0) {
+    unhideMainAndFooter();
+  } else {
+    hideMainAndFooter();
+  }
+}
 
-console.log(sayHello("Hello"));
+function hideMainAndFooter() {
+  node.main.classList.add("hidden");
+  node.footer.classList.add("hidden");
+}
+
+function unhideMainAndFooter() {
+  node.main.classList.remove("hidden");
+  node.footer.classList.remove("hidden");
+}
+
+checkTodoTask(node.todoList);
---
+++ src/js/nodes.js
@@ -0,0 +1,12 @@
+const $ = (selector) => document.querySelector(selector);
+const create = (element) => document.createElement(element);
+
+export const main = $(".main");
+export const todoList = $(".todo-list");
+export const footer = $(".footer");
+export const todoCount = $(".todo-count");
+export const filters = $(".filters");
+
+// buttons
+
+export const clearCompleted = $(".clear-completed");
